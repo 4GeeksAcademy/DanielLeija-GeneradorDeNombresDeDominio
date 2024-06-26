088b922f84de2f0228230f77897e343047aa62a1
dleija01 <168392168+dleija01@users.noreply.github.com>
2024-06-26 03:25:29 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let articles = ["the", "an", "some", "another", "any"];
+  let adjectives = ["awesome", "cool", "intriguing", "thrilling", "funny"];
+  let nouns = ["website", "blog", "webapp", "spot", "hub"];
+  let tlds = [".com", ".de", ".net", ".org", ".mx", ".us"];
+
+  let domainNames = [];
+
+  for (let article of articles) {
+    for (let adjective of adjectives) {
+      for (let noun of nouns) {
+        for (let tld of tlds) {
+          domainNames.push(`${article}${adjective}${noun}${tld}`);
+        }
+      }
+    }
+  }
+  document.querySelector("#generate").onclick = () => {
+    document.querySelector("#domain").innerHTML = `<h1>${
+      domainNames[Math.floor(Math.random() * domainNames.length)]
+    } </h1>`;
+  };
 };
